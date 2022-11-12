8a685269e21a9140c68f7c57c5a8c15e78096118
Marek <mareksm99@gmail.com>
2022-11-12 18:42:44 +0100

    Expand dictionary

--- src/Dictionary.py
+++ src/Dictionary.py
@@ -8,6 +8,8 @@ class Dictionary:
     def __init__(self):
         self.color = (15,86,135,255)
         self.checkboxes = []
+        self.yOffset = 100
+        self.xOneOffset = 5
         with dpg.window(label="Dictionary", autosize=True, tag="dictionary", pos=[99999, 99999],
                         on_close=lambda: dpg.show_item("mainWindow")):
             dpg.hide_item("dictionary")
@@ -36,7 +38,7 @@ class Dictionary:
 
 
                     with dpg.table_cell():
-                        with dpg.drawlist(width=800, height=300):
+                        with dpg.drawlist(width=800, height=500):
                             with dpg.draw_layer():
                                 dpg.draw_line((48, 50), (653, 50), color=self.color, thickness=5)
                                 dpg.draw_line((50, 50), (50, 103), color=self.color, thickness=5)
@@ -53,10 +55,10 @@ class Dictionary:
                                 dpg.draw_text((164, 54), "0", color=(250, 250, 250, 255), size=50)
 
                                 dpg.draw_line((250, 50), (250, 100), color=self.color, thickness=5)
-                                dpg.draw_text((214, 54), "0", color=(250, 250, 250, 255), size=50)
+                                dpg.draw_text((214 + self.xOneOffset, 54), "1", color=(250, 250, 250, 255), size=50)
 
                                 dpg.draw_line((300, 50), (300, 100), color=self.color, thickness=5)
-                                dpg.draw_text((264, 54), "0", color=(250, 250, 250, 255), size=50)
+                                dpg.draw_text((264 + self.xOneOffset, 54), "1", color=(250, 250, 250, 255), size=50)
 
                                 dpg.draw_line((350, 50), (350, 100), color=self.color, thickness=5)
                                 dpg.draw_text((314, 54), "0", color=(250, 250, 250, 255), size=50)
@@ -65,7 +67,7 @@ class Dictionary:
                                 dpg.draw_text((364, 54), "0", color=(250, 250, 250, 255), size=50)
 
                                 dpg.draw_line((450, 50), (450, 100), color=self.color, thickness=5)
-                                dpg.draw_text((414, 54), "0", color=(250, 250, 250, 255), size=50)
+                                dpg.draw_text((414 + self.xOneOffset, 54), "1", color=(250, 250, 250, 255), size=50)
 
                                 dpg.draw_line((500, 50), (500, 100), color=self.color, thickness=5)
                                 dpg.draw_text((464, 54), "0", color=(250, 250, 250, 255), size=50)
@@ -76,14 +78,58 @@ class Dictionary:
                                 dpg.draw_line((600, 50), (600, 100), color=self.color, thickness=5)
                                 dpg.draw_text((564, 54), "0", color=(250, 250, 250, 255), size=50)
 
-                                dpg.draw_text((614, 54), "0", color=(250, 250, 250, 255), size=50)
+                                dpg.draw_text((614 + self.xOneOffset, 54), "1", color=(250, 250, 250, 255), size=50)
+
+                            with dpg.draw_layer():
+                                dpg.draw_line((48, 50 + self.yOffset), (653, 50 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_line((50, 50 + self.yOffset), (50, 103 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_line((50, 100 + self.yOffset), (653, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_line((650, 100 + self.yOffset), (650, 50 + self.yOffset), color=self.color, thickness=5)
+
+                                dpg.draw_line((100, 50 + self.yOffset), (100, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((64 + self.xOneOffset, 54 + self.yOffset), "1", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((150, 50 + self.yOffset), (150, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((114 + self.xOneOffset, 54 + self.yOffset), "1", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((200, 50 + self.yOffset), (200, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((164, 54 + self.yOffset), "0", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((250, 50 + self.yOffset), (250, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((214 + self.xOneOffset, 54 + self.yOffset), "1", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((300, 50 + self.yOffset), (300, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((264, 54 + self.yOffset), "0", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((350, 50 + self.yOffset), (350, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((314, 54 + self.yOffset), "0", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((400, 50 + self.yOffset), (400, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((364, 54 + self.yOffset), "0", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((450, 50 + self.yOffset), (450, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((414 + self.xOneOffset, 54 + self.yOffset), "1", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((500, 50 + self.yOffset), (500, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((464 + self.xOneOffset, 54 + self.yOffset), "1", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((550, 50 + self.yOffset), (550, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((516 + self.xOneOffset, 54 + self.yOffset), "1", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_line((600, 50 + self.yOffset), (600, 100 + self.yOffset), color=self.color, thickness=5)
+                                dpg.draw_text((564, 54 + self.yOffset), "0", color=(250, 250, 250, 255), size=50)
+
+                                dpg.draw_text((614, 54 + self.yOffset), "0", color=(250, 250, 250, 255), size=50)
 
                             with dpg.draw_layer(tag="genLayer", show=False):
                                 #dpg.draw_arrow((50, 70), (100, 65), color=(0, 200, 255), thickness=1, size=10)
-                                dpg.draw_circle((125, 75), color=(255, 0, 0, 50), radius=38, thickness=5)
+                                dpg.draw_circle((125, 75), color=(255, 0, 0, 100), radius=38, thickness=5)
 
                             with dpg.draw_layer(tag="chromoLayer", show=False):
-                                dpg.draw_ellipse((25, 25), (675, 125), color=(255, 0, 0, 50), thickness=5)
+                                dpg.draw_ellipse((25, 25), (675, 125), color=(255, 0, 0, 100), thickness=5)
+
+                            with dpg.draw_layer(tag="popuLayer", show=False):
+                                dpg.draw_ellipse((25, 25), (675, 125 + self.yOffset), color=(255, 0, 0, 100), thickness=5)
 
     def show(self):
         if not dpg.is_item_visible("dictionary"):
@@ -104,7 +150,10 @@ class Dictionary:
             if c == sender:
                 if i ==0:
                     dpg.configure_item("genLayer", show=show_value)
-                elif i == 2 or i ==1:
+                elif i == 2 or i == 1:
+                    show_value = dpg.get_value(self.checkboxes[2]) or dpg.get_value(self.checkboxes[1])
                     dpg.configure_item("chromoLayer", show=show_value)
+                elif i == 3:
+                    dpg.configure_item("popuLayer", show=show_value)
             i += 1
 
